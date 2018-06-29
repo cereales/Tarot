92136030bf658e1bea787883013144f6298a8994
Paul Bourgeois <paul.bourgeois@sap.com>
2018-06-29 14:58:12 +0200

    Ajout d'un type abstrait de test

---
+++ src/main/java/game/tests/AbstractTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.tests;
+
+/**
+ * Allows tests to use ASSERT_xx functions.
+ *
+ * @author paul
+ */
+public abstract class AbstractTest {
+    public AbstractTest() {
+    }
+    
+    /**
+     * Allows to test with assert functions.
+     */
+    public void test_assert() {
+    }
+
+    public static void ASSERT_EQ(int num1, int num2) {
+    }
+}
