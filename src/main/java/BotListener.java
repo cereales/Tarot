065f7973d5a889936192699a4819ea43e6d33208
Paul Ghib <paul.bourgeois@ensimag.fr>
2018-08-16 19:20:55 +0200

    * [discord] Set up tools for discord connexion

---
+++ src/main/java/BotListener.java
@@ -0,0 +1,115 @@
+/*
+ *     Copyright 2015-2018 Austin Keener & Michael Ritter & Florian Spieß
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fr.connexion.PrivateInformations;
+import net.dv8tion.jda.client.entities.Group;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.PermissionException;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+import java.util.Random;
+
+public class BotListener extends PrivateInformations
+{
+    private static String C_BASIC = "\033[0m";
+    private static String C_YELLOW = "\033[33m";
+    private static String C_RED = "\033[31m";
+    private static String C_BLUE = "\033[34m";
+
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken(token)           //The token of the account that is logging in.
+                    .addEventListener(new BotListener())  //An instance of a class that will handle events.
+                    .buildBlocking();  //There are 2 ways to login, blocking vs async. Blocking guarantees that JDA will be completely loaded.
+        }
+        catch (LoginException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        JDA jda = event.getJDA();                       //JDA, the core of the api.
+        long responseNumber = event.getResponseNumber();//The amount of discord events that JDA has received since the last reconnect.
+
+        //Event specific information
+        User author = event.getAuthor();                //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
+                                                        //  This could be a TextChannel, PrivateChannel, or Group!
+
+        String msg = message.getContentDisplay();              //This returns a human readable version of the Message. Similar to
+                                                        // what you would see in the client.
+
+        boolean bot = author.isBot();                    //This boolean is useful to determine if the User that
+                                                        // sent the Message is a BOT or not!
+
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
+        {
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (note, in the API, Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
+
+            String name;
+            if (message.isWebhookMessage())
+            {
+                name = author.getName();                //If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
+            else
+            {
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
+
+            System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
+        }
+        else if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        {
+            PrivateChannel privateChannel = event.getPrivateChannel();
+
+            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+        }
+        else if (event.isFromType(ChannelType.GROUP))   //If this message was sent to a Group. This is CLIENT only!
+        {
+            Group group = event.getGroup();
+            String groupName = group.getName() != null ? group.getName() : "";  //A group name can be null due to it being unnamed.
+
+            System.out.printf("[GRP: %s]<%s>: %s\n", groupName, author.getName(), msg);
+        }
+
+
+        if (msg.equals("!ping"))
+        {
+            channel.sendMessage("pong!").queue();
+        }
+    }
+}
